e05a656b07623bd337c7d9c4237090cf8c7f317b
Admiral <Admiral@ex.com>
2026-02-13 01:52:35 +0300

    Step6_db

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,6 @@
 from flask import Flask
+import psycopg2
+import os
 
 app = Flask(__name__)
 
@@ -8,3 +10,23 @@ def health():
 
 if __name__=="__main__":
     app.run(host="0.0.0.0", port=5000)
+
+def get_db_connection():
+    conn = psycopg2.connect(
+        host="db",
+        database="asm_database",
+        user="asm_user",
+        password="asm_password"
+    )
+    return conn
+
+@app.route("/db-test")
+def db_test():
+    conn = get_db_connection()
+    cur = conn.cursor()
+    cur.execute("SELECT 1;")
+    result = cur.fetchone()
+    cur.close()
+    conn.close()
+    return {"db_response": result[0]}
+    
