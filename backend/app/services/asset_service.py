1f2615cd8f4c503f95fabe13c22438550e2bf35e
Admiral <Admiral@ex.com>
2026-02-14 20:28:07 +0300

    Step17_assets_ro

---
+++ backend/app/services/asset_service.py
@@ -0,0 +1,19 @@
+from app.services.db_service import get_db_connection
+
+def create_asset(user_id, domin, ip_address):
+    conn = get_db_connection
+    cur = cursor()
+    cur.execute(
+        """
+        INSERT INTO assets (user_id, domain, ip_address)
+        VALUES (%s, %s, %s)
+        RETURNING id;
+        """
+    ,
+    (user_id,domin,ip_address)
+    )
+asset_id = cur.fetchone()[0]
+conn.commit()
+cur.close()
+conn.close()
+return asset_id
