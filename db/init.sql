52134481d9adb0fc9af04ad0ae506cbd32d5c3df
Admiral <Admiral@ex.com>
2026-02-14 19:22:10 +0300

    Step17_Auth_db

--- db/init.sql
+++ db/init.sql
@@ -6,3 +6,11 @@ CREATE TABLE IF NOT EXISTS users (
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
+اظن ان الامور كلها تمام 
+admiral@admiral-vivo:~$ curl -X POST http://172.18.0.4:5000/api/register -H "Content-Type: application/json" -d '{"email":"admin@test.com","password":"123456"}'
+{"message":"User created with id 1"}
+
+الان مارايك ان نكمل بناء المشروع انا لا اريد حاليا توسيعه كثيرا فقط الالتزام في المطلوب لانه مشروع في كورس 
+للتأكيد المطلوب في المرفق
+
+ما الخطوات القادمة؟؟ بشكل احترافي وبدون توسع مع الالترام بالمطلوب
